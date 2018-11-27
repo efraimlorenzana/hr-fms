cf4f269729f9d812760f712b2cdc9e0eb5d71582
Efraim A. Lorenzan <efraim.lorenzana@gmail.com>
2018-11-27 17:34:51 +0800

    modify env and database.php file to hide passwords

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'db_file'),
             'username' => env('DB_USERNAME', 'efraimlorenzana'),
-            'password' => env('DB_PASSWORD', 'Em052011@61912716'),
+            'password' => env('DB_PASSWORD', '*****'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
